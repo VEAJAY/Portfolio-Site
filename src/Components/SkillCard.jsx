96af7f0560daabe3b4b4db14b4b7829b119a3447
VEAJAY <veaseyjayland3@gmail.com>
2024-10-21 23:45:34 -0500

    Props validation

--- src/Components/SkillCard.jsx
+++ src/Components/SkillCard.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 const SkillCard = ({ skill, imageSrc, bgColor }) => {
     return (
         <div className={`flex flex-col items-center justify-center h-40 rounded-lg text-center ${bgColor} dark:bg-slate-400 p-2`}>
@@ -5,6 +7,16 @@ const SkillCard = ({ skill, imageSrc, bgColor }) => {
             <span className="text-white dark:text-gray-900">{skill}</span>
         </div>
     );
+};
+
+SkillCard.propTypes = {
+    skill: PropTypes.string.isRequired,
+    imageSrc: PropTypes.string.isRequired,
+    bgColor: PropTypes.string,
+};
+
+SkillCard.defaultProps = {
+    bgColor: 'bg-slate-700',
 };
 
 export default SkillCard;
