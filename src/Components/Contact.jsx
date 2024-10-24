ed2f040438262a07a23aac9bcd1e0cf238700853
VEAJAY <veaseyjayland3@gmail.com>
2024-10-23 20:38:33 -0500

    Contact page elements added

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,14 +1,48 @@
+import ContactCard from './ContactCard';
 import Title from './Title';
 
 function Contact() {
   return (
-    <div className="min-h-screen dark:bg-gray-200">
+    <div className="h-screen dark:bg-gray-200">
         
         <Title 
           title="Contact"
           description="Feel free to reach out"
         />
 
+        <div className="flex justify-center">
+          <div className="grid grid-cols-1 sm:grid-cols-3 gap-24 justify-center max-w-6xl px-4">
+
+            <ContactCard 
+              image="../src/assets/socials_logo/email-logo.png"
+              note="Send me an email"
+              contact="veaseyjayland3@gmail.com"
+              link=""
+            />
+
+            <ContactCard 
+              image="../src/assets/socials_logo/twitter-logo.png"
+              note="My Twitter / X account"
+              contact="@jayland37"
+              link="https://x.com/jayland37"
+            />
+
+            <ContactCard 
+              image="../src/assets/socials_logo/linkedin-logo.png"
+              note="View my Linkedin account"
+              contact="@Jayland Veasey"
+              link="https://www.linkedin.com/in/jayland-veasey-72696715b/"
+            />
+
+            <ContactCard 
+              image="../src/assets/socials_logo/githubdesktop-logo.webp"
+              note="Check out my GitHub account"
+              contact="@VEAJAY"
+              link="https://github.com/VEAJAY"
+            />
+
+          </div>
+        </div>
 
     </div>
   )
---
+++ src/Components/ContactCard.jsx
@@ -0,0 +1,21 @@
+const ContactCard = ({ image, note, contact, link }) => {
+    return (
+        <div className="flex flex-row items-center">
+                <img src={image} className="w-16 h-16"/>
+
+                <div className="flex flex-col ml-4 text-white dark:text-gray-900">
+                    <a href={link} target="_blank">
+                        <p className="text-gray-400 font-thin dark:text-black">
+                            {note}
+                        </p>
+                        <h2 className="font-bold text-xl">
+                            {contact}
+                        </h2>
+                    </a>
+                    
+                </div>
+            
+        </div>
+    );
+}
+export default ContactCard
